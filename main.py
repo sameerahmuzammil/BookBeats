250467cbedba5973900923362ea401bcb2cc404c
sameerahmuzammil <sameerah.muzammil@gmail.com>
2026-07-27 18:27:17 +0500

    Fixed Jinja2Template issue

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -9,6 +9,18 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def read_home(request: Request):
-    return templates.TemplateResponse("home.html", {"request":request})
+@app.get("/", include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html")
+
+@app.get("/library", include_in_schema=False)
+def library(request: Request):
+    return templates.TemplateResponse(request, "library.html")
+
+@app.get("/packages", include_in_schema=False)
+def packages(request: Request):
+    return templates.TemplateResponse(request, "packages.html")
+
+@app.get("/dashboard", include_in_schema=False)
+def dashboard(request: Request):
+    return templates.TemplateResponse(request, "dashboard.html")
