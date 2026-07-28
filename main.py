500d29aae4c095ecb0a7258a17929f4b1112dcce
sameerahmuzammil <sameerah.muzammil@gmail.com>
2026-07-28 15:21:14 +0500

    Dummy List and Home Page Update

--- main.py
+++ main.py
@@ -9,9 +9,48 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
+books = [
+    {
+        'id': 1,
+        'book_name': 'Eleanor And Park',
+        'author': 'Rainbow Rowell',
+        'cover_url': 'https://picsum.photos',
+        'total_pages': 476,
+        'status': 'Currently Reading',
+        'current_page': 145,
+        'last_read': '2026-07-26',
+        'user_rating': 4,
+        'favorite': False
+    },
+    {
+        'id': 2,
+        'book_name': 'Better Than the Movies',
+        'author': 'Lynn Painter',
+        'cover_url': 'https://picsum.photos',
+        'total_pages': 368,
+        'status': 'Currently Reading',
+        'current_page': 203,
+        'last_read': '2026-07-26',
+        'user_rating': 5,
+        'favorite': True
+    },
+    {
+        'id': 3,
+        'book_name': 'Six of Crows',
+        'author': 'Leigh Bardugo',
+        'cover_url': 'https://picsum.photos',
+        'total_pages': 496,
+        'status': 'To Be Read',
+        'current_page': 0,
+        'last_read': None,
+        'user_rating': 4,
+        'favorite': False
+    }
+]
+
 @app.get("/", include_in_schema=False)
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html")
+    return templates.TemplateResponse(request, "home.html", {'books': books})
 
 @app.get("/library", include_in_schema=False)
 def library(request: Request):
